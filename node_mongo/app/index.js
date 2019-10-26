f6cb23cf18ed293c000ae42e44af75350baa1934
Ali Alp <alialp3.141@gmail.com>
2019-10-26 18:57:34 +0300

    working with mongo ready to customize

--- node_mongo/app/index.js
+++ node_mongo/app/index.js
@@ -1,15 +1,27 @@
 import express from 'express';
-import routes from './route';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './routes/contactRoutes';
 
 const app = express();
 const PORT = 3000;
 
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/conatctDb', {
+    useMongoClient: true
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 routes(app);
 
 app.get('/', (req, res) =>
-        res.send(`Node and express server is running on port ${PORT}`)
+    res.send(`Node and express server is running on port ${PORT}`)
 );
 
 app.listen(PORT, () =>
-        console.log(`your server is running on port ${PORT}`)
+    console.log(`your server is running on port ${PORT}`)
 );
